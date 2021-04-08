c030742b2c61408f864f32e5146b9e5ba670fcff
kuzoki <kuzokio4@gmail.com>
2021-04-08 20:46:52 +0000

    first

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name: FaceBook  Page Feed
+ * Plugin URI: 
+ * Description: Display The Page Feed in Widgets
+ * Version: 1.0.0
+ * Author: Abderrahmane Bamoussa
+ * Email : kuzokio4@gmail.com
+ * Author URI: 
+ */
+
+if (!defined('ABSPATH')) {
+	# code...
+	exit;
+}
+
+
+require_once(plugin_dir_path( __FILE__ ).'/inc/facebook-wid.php');
